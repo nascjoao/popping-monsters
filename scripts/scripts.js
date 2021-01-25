e4b48ee5cc70c59e7ed6497851b2fda073dd1332
nascjoao <oi@joaonasc.dev>
2021-01-25 13:45:36 -0300

    set offline fallback

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
+window.addEventListener('load', () => {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register('./service-worker.js')
+    }
+})
+
 const monsters = ['./images/monster.gif']
 
 const scoreElement = document.getElementById('score')
