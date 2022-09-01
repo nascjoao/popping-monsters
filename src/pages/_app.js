58f50da5123fba801fc1cf08367d5cbb705121bf
nascjoao <oi@joaonasc.dev>
2022-09-01 13:00:07 -0300

    feat: create app component

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/base.css'
+import '../styles/game.css'
+import '@fontsource/luckiest-guy'
+
+export default function App({ Component, props }) {
+  return (
+    <Component {...props} />
+  )
+}
