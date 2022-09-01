481e23d39081076e90c618ac15061c216cf29504
nascjoao <oi@joaonasc.dev>
2022-09-01 13:03:57 -0300

    feat: add head info to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,20 @@
+import Head from 'next/head'
 import '../styles/base.css'
 import '../styles/game.css'
 import '@fontsource/luckiest-guy'
 
 export default function App({ Component, props }) {
   return (
-    <Component {...props} />
+    <>
+      <Head>
+        <link rel="shortcut icon" href="./images/monster.gif" type="image/x-icon" />
+        <link rel="manifest" href="./manifest.webmanifest" />
+        <title>Popping Monsters</title>
+        <meta name="theme-color" content="#156E09" />
+        <meta name="msapplication-navbutton-color" content="#156E09" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#156E09" />
+      </Head>
+      <Component {...props} />
+    </>
   )
 }
