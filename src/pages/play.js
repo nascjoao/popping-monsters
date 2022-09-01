22d70dae6ed2878378815f3901d987eb6d9db80e
nascjoao <oi@joaonasc.dev>
2022-09-01 16:20:45 -0300

    feat: spawn monsters

--- src/pages/play.js
+++ src/pages/play.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import useMonsters from '../hooks/useMonsters'
 
 export default function Play() {
+  const { monsters, spawnMonsters, hitMonster, survivingMonsters, killedMonsters } = useMonsters()
+
+  useEffect(() => {
+    spawnMonsters(1200, 5)
+  }, [])
+
   return (
-    <div>Play</div>
+    <>
+      <h1>Monstros eliminados: {killedMonsters.length}</h1>
+      <h1>Monstros sobreviventes: {survivingMonsters.length}</h1>
+      { monsters.map((monster) => (
+        <img key={monster.id} src={monster.src} alt="monster" style={{
+          position: 'absolute',
+          top: `${monster.y}px`,
+          left: `${monster.x}px`,
+          width: '4rem',
+          zIndex: '2'
+        }}
+        onMouseOver={() => hitMonster(monster.id)}
+        />
+      )) }
+    </>
   )
 }
